90fb706d966e4d40de815fef206ac20fee7daa13
uriya-monko <uriyamonko@gmail.com>
2025-02-18 12:00:05 +0200

    Add files via upload

---
+++ myApp/myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Israel4 on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myApp/myAppTests/myAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  myAppTests.swift
+//  myAppTests
+//
+//  Created by Israel4 on 18/02/2025.
+//
+
+import Testing
+@testable import myApp
+
+struct myAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
